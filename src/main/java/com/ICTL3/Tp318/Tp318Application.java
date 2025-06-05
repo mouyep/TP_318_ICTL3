f9266b9267a91a383d6e74208db30f0069f7383c
mouyep <mouyepalain9@gmail.com>
2025-06-05 12:48:18 +0200

    setting projetct TP 3018

---
+++ src/main/java/com/ICTL3/Tp318/Tp318Application.java
@@ -0,0 +1,13 @@
+package com.ICTL3.Tp318;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp318Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp318Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ICTL3/Tp318/Tp318ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ICTL3.Tp318;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp318ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
